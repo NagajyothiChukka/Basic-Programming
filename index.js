b35f3d8543838f98295c67452616deb0504d341a
NagajyothiChukka <nagajyotichukka@gmail.com>
2024-12-08 09:40:01 +0530

    added evenOdd

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ function checkPrime(num) {
     return true;
 }
 console.log(checkPrime(8));
+
+function evenOdd(num) {
+      
+      // checks if its even number
+        if(num % 2 === 0) {
+            return true; // Even number
+        } else {
+            return false; // odd number
+        }
+      
+}
+console.log(evenOdd(4));
+console.log(evenOdd(1));
